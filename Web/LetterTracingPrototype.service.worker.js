98f7c9bf389dfd38a58780e5eafd264e0e2cbf74
Sambhav Aswal <sambhav.aswal@magicedtech.com>
2025-04-29 22:50:25 +0530

    Thread support enabled. Canvas Resize Policy set back to Adaptive.

--- Web/LetterTracingPrototype.service.worker.js
+++ Web/LetterTracingPrototype.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1745946726|16981889684';
+const CACHE_VERSION = '1745947173|17428477592';
 /** @type {string} */
 const CACHE_PREFIX = 'Letter Tracing P-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
